1c324311c40fd3727dd3c0ca1fb5111d96e386d3
kmdouglass <kyle.m.douglass@gmail.com>
2014-10-07 15:48:44 +0200

    Added bump function to simExperiment class.

--- clusterAnalysis.py
+++ clusterAnalysis.py
@@ -9,7 +9,7 @@ microscopy experiments.
 
 __author__ = 'Kyle M. Douglass'
 __email__ = 'kyle.m.douglass@gmail.com'
-__version__ = 0.2
+__version__ = 0.3
 
 import numpy as np
 
@@ -39,9 +39,12 @@ class simExperiment():
         if self.distr == 'gaussian':
             self.points = clusterWidth * np.random.randn(ptsPerCluster, 3, numClusters)
         elif self.distr == 'uniform':
-            self.points = 2 * clusterWidth * (np.random.rand(ptsPerCluster, 3, numClusters) - 1)
+            self.points = 2 * clusterWidth * (np.random.rand(ptsPerCluster, 3, numClusters) - 0.5)
 
     def findRadGyr(self):
+        """Finds the radius of gyration of each cluster.
+
+        """
         centers = np.mean(self.points, axis = 0)
         deltaR = self.points - centers
 
@@ -49,7 +52,42 @@ class simExperiment():
 
         return np.sqrt(Rg2)
 
+    def bumpPoints(self, locPrec):
+        """Randomly bumps the position of all points.
+
+        This function simulates the actual measurement of the points
+        within a cluster by bumping the positions of each point randomly
+        in each direction. The distance bumped in each direction is
+        determined from a multi-modal Gaussian distribution whose
+        standard deviations are equivalent to the localization precision
+        in x, y, and z.
+
+        Parameters
+        ---------
+        locPrec : tuple of floats
+            Contains the localization precision in x, y, and z.
+
+        Returns
+        -------
+        bumpedPoints : ndarray
+
+        """
+        pointShape = self.points.shape
+        bumpDistX = locPrec[0] * np.random.randn(ptsPerCluster, numClusters)
+        bumpDistY = locPrec[1] * np.random.randn(ptsPerCluster, numClusters)
+        bumpDistZ = locPrec[2] * np.random.randn(ptsPerCluster, numClusters)
+
+        bumpedPoints = np.zeros(self.points.shape)
+        bumpedPoints[:,0,:] = self.points[:,0,:] + bumpDistX
+        bumpedPoints[:,1,:] = self.points[:,1,:] + bumpDistY
+        bumpedPoints[:,2,:] = self.points[:,2,:] + bumpDistZ
+
+        return bumpedPoints
+
 if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+
     numClusters = 10
     ptsPerCluster = 1000
     clusterWidth = 200
@@ -57,8 +95,14 @@ if __name__ == '__main__':
     
     myExp = simExperiment(numClusters, ptsPerCluster, clusterWidth, distr)
     Rg = myExp.findRadGyr()
-    print(Rg)
+    bp = myExp.bumpPoints((1,1,1))
+    
+    fig = plt.figure(figsize=(8,8))
+    ax = fig.add_subplot(111, projection='3d')
 
+    ax.scatter(myExp.points[:,0,0], myExp.points[:,1,0], myExp.points[:,2,0], color = 'blue')
+    ax.scatter(bp[:,0,0], bp[:,1,0], bp[:,2,0], color = 'red')
+    plt.show()
 
 
 
